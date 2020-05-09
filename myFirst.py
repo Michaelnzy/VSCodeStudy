420b6bf88d8d5257d6b2c9f6e7f287708b43a37f
Michaelnzy <448454156@qq.com>
2020-05-09 15:10:21 +0800

    '增加第二个窗体'

--- myFirst.py
+++ myFirst.py
@@ -1,2 +1,7 @@
-print("hellow")
-print("中文")
+import wx                               #导入wx包
+
+app = wx.App()                          #创建应用程序对象
+win = wx.Frame(None,-1,'install test')  #创建窗体
+btn = wx.Button(win, label = 'AAAA')  #创建Button
+win.Show()                              #显示窗体
+app.MainLoop()  
